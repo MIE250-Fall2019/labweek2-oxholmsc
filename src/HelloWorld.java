fa8ec4c12d6d74cc912ae5cb1cac6422569ae8ef
Scott Oxholm <oxholmsc@WIN.ecf.utoronto.ca>
2019-09-12 16:25:30 -0400

    done with errors

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,7 @@
 public class HelloWorld {
-	
+    public static void main(String[] args){
+        //printin will print everything on the same line 
+        //println will print each thing on a new line
+        System.out.println("Hello World");
+    }
 }
